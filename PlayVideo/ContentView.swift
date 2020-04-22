ea90ee2048d6f3d54ac7f9464003fe1b4d616bab
dirk-bester <3698896+dirk-bester@users.noreply.github.com>
2020-04-21 23:47:46 -0700

    Initial Commit

---
+++ PlayVideo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlayVideo
+//
+//  Created by Dirk Bester on 4/21/20.
+//  Copyright © 2020 Dirk Bester. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
